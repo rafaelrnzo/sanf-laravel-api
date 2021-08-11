24ce8c0eba92b7f7b0a2c59e8f12eee01364cda3
gozals25 <gozals25@nusantarabetastudio.com>
2021-08-11 16:00:41 +0700

    fix(pages): add pages path param instead of web-view
    Took 12 minutes
    
    
    Took 25 seconds

--- packages/sanf/web/src/routes.php
+++ packages/sanf/web/src/routes.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('web-view/about-us', ['as' => 'web-view.about-us', 'uses' => 'Common\WebViewController@aboutUs']);
 Route::get('web-view/terms-and-condition', ['as' => 'web-view.terms-and-condition', 'uses' => 'Common\WebViewController@termsCondition']);
 Route::get('web-view/privacy-policy', ['as' => 'web-view.privacy-policy', 'uses' => 'Common\WebViewController@privacyPolicy']);
+
+Route::get('pages/about-us', ['as' => 'web-view.about-us', 'uses' => 'Common\WebViewController@aboutUs']);
+Route::get('pages/terms-and-condition', ['as' => 'web-view.terms-and-condition', 'uses' => 'Common\WebViewController@termsCondition']);
+Route::get('pages/privacy-policy', ['as' => 'web-view.privacy-policy', 'uses' => 'Common\WebViewController@privacyPolicy']);
