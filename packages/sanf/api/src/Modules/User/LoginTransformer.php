608543089fc5b688c0a01da973b57c35f77140e8
gozals25 <gozals25@nusantarabetastudio.com>
2021-08-16 12:30:56 +0700

    fix(login): handle empty type_id
    Took 24 minutes

--- packages/sanf/api/src/Modules/User/LoginTransformer.php
+++ packages/sanf/api/src/Modules/User/LoginTransformer.php
@@ -14,11 +14,11 @@ class LoginTransformer extends TransformerAbstract
         return [
             'profile' => [
                 'id' => $item->id,
-                'xid' => $item->xid,
+                'xid' => (string)$item->xid,
                 'full_name' => $item->full_name,
                 'email' => $item->username,
-                'type_name' => (new ProfileType($item->profile_type))->getTranslation(),
-                'type_id' => $item->profile_type
+                'type_name' => (new ProfileType($item->profile_type ?? ProfileType::PERSONAL))->getTranslation(),
+                'type_id' => $item->profile_type ?? ProfileType::PERSONAL
             ]
         ];
     }
--- packages/sanf/core/src/Modules/User/SwitchActiveCustomerProfileService.php
+++ packages/sanf/core/src/Modules/User/SwitchActiveCustomerProfileService.php
@@ -4,7 +4,6 @@
 namespace Sanf\Core\Modules\User;
 
 
-use Carbon\Carbon;
 use NbsPhp\Core\Exceptions\UserNotFoundException;
 use NbsPhp\Core\Models\AuthModel;
 use NbsPhp\Core\Services\ApplicationServiceInterface;
@@ -37,36 +36,5 @@ class SwitchActiveCustomerProfileService implements ApplicationServiceInterface
         $user->xid = $response['data'][0]['CUST_ID_SANF'];
         $user->profile_type = $response['data'][0]['ID_IDENTITY'];
         $user->save();
-
-        return collect($response['data'])
-            ->map(function ($item){
-                return (object)[
-                    "xid" => $item['CUST_ID_SANF'],
-                    "typeId" => $item['ID_IDENTITY'],
-                    "typeName" => $item['DESC_IDENTITY'],
-                    "title" => $item['COMPANY_TYPE'],
-                    "fullName" => $item['IDENTITY_NAME'],
-                    "picName" => $item['PIC_NAME'],
-                    "identityNumber" => $item['KTP'],
-                    "npwp" => $item['NPWP'],
-                    "email" => $item['EMAIL_ADDR'],
-                    "landlineNumber" => $item['NO_TELP'],
-                    "phoneNumber" => $item['NO_HP'],
-                    "gender" => $item['GENDER'],
-                    "birthdate" => Carbon::make($item['TGL_LAHIR']),
-                    "countryId" => $item['ID_NEGARA'],
-                    "countryName" => $item['NEGARA'],
-                    "provinceId" => $item['ID_PROVINSI'],
-                    "provinceName" => $item['PROVINSI'],
-                    "cityId" => $item['ID_KOTA'],
-                    "cityName" => $item['KOTA'],
-                    "districtName" => $item['KECAMATAN'],
-                    "subdistrictName" => $item['KELURAHAN'],
-                    "postcode" => $item['KODEPOS'],
-                    "address" => $item['ALAMAT'],
-                    "businessSince" => $item['LAMA_USAHA'],
-                    "isPic" => $item['PIC']
-                ];
-            });
     }
 }
