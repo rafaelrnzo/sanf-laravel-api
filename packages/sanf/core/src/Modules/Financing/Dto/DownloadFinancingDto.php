845a3a2bb22a3845bff45a4b18e6c8e26234f25f
Imam Taufiq Ponco <ponco@nbs.co.id>
2021-10-28 17:05:46 +0700

    fix(financing): download financing simulation sample download

---
+++ packages/sanf/core/src/Modules/Financing/Dto/DownloadFinancingDto.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Sanf\Core\Modules\Financing\Dto;
+
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class DownloadFinancingDto extends DataTransferObject
+{
+    public ?object $result;
+}
