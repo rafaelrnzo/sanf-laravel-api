e9b49695d17faaced12076c783ec5252f7bf142f
Imam Taufiq Ponco <ponco@nbs.co.id>
2021-10-28 13:29:06 +0700

    feat(financing): add Download financing simulation service

---
+++ packages/sanf/core/src/Modules/Financing/Services/DownloadFinancingSimulationService.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Sanf\Core\Modules\Financing\Services;
+
+
+use NbsPhp\Core\Services\ApplicationServiceInterface;
+
+class DownloadFinancingSimulationService extends FinancingService implements ApplicationServiceInterface
+{
+    public function execute($dto = null)
+    {
+       // TODO : Download PDF Simulation
+    }
+
+}
