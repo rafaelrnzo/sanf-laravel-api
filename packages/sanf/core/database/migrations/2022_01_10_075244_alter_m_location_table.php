076ae8e15ecb8b6fbf41ab99309b013a56ffdece
nbdiar <diar@nusantarabetatstudio.com>
2022-01-10 11:56:03 +0700

    [MODIFIED] master location v2
    Took 17 minutes

---
+++ packages/sanf/core/database/migrations/2022_01_10_075244_alter_m_location_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AlterMLocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('m_location');
+        Schema::create('m_location', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('xid', 32);
+            $table->string('name', 128);
+            $table->smallInteger('level');
+            $table->smallInteger('depth');
+            $table->smallInteger('parent_id')->nullable();
+            $table->smallInteger('postal_code')->nullable();
+            $table->integer('sort')->default(1);
+            $table->json('metadata');
+            $table->timestampTz('created_at', 6)->nullable()->default((DB::raw('CURRENT_TIMESTAMP')));
+            $table->timestampTz('updated_at', 6)->nullable()->default((DB::raw('CURRENT_TIMESTAMP')));
+            $table->json('modified_by');
+            $table->bigInteger('version')->unsigned()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ packages/sanf/core/database/seeds/LocationV2Seeder.php
@@ -0,0 +1,65 @@
+<?php
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Sanf\Core\Modules\Location\AdministrativeAreaModel;
+use Sanf\Core\Modules\Location\LocationModel;
+
+class LocationV2Seeder extends Seeder
+{
+    const FILES = [
+        'm_location_v2.json' => LocationModel::class,
+    ];
+    protected $chunk = 1000;
+
+    /**
+     * Run the database seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::beginTransaction();
+        try {
+            $header = ['TABLE', 'JSON', 'EXECUTION TIME'];
+            $body = [];
+            $executionSeedStartTime = microtime(true);
+
+            /* @var Model $model */
+            foreach (self::FILES as $file => $model) {
+                $this->command->getOutput()->writeln("<comment>Seeding:</comment> {$file}");
+                $datas = json_decode(File::get("database/data/{$file}"), true);
+
+                $startTime = microtime(true);
+                $datas = collect($datas);
+                $chunkedData = $datas->chunk($this->chunk);
+                $this->command->getOutput()->progressStart(count($chunkedData));
+                foreach ($chunkedData as $data) {
+                    $model::query()->insert($data->toArray());
+                    $this->command->getOutput()->progressAdvance();
+                }
+
+                $this->command->getOutput()->progressFinish();
+                $runTime = round(microtime(true) - $startTime, 2);
+                $body[] = [(new $model)->getTable(), $file, "{$runTime} seconds"];
+            }
+
+            $executionSeedEndTime = microtime(true);
+
+            $seedTime = round($executionSeedEndTime - $executionSeedStartTime, 2);
+            $body[] = ['ALL TABLE', '-', "{$seedTime} seconds"];
+            $this->command->table($header, $body);
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            report($e);
+            dump($e);
+            die;
+        }
+
+    }
+}
