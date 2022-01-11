c54f0e3956f2762a0a6e896678f21bf1c6cb7e9b
gozals25 <gozals25@nusantarabetastudio.com>
2022-01-11 12:14:37 +0700

    feat: add public api push notif for trial
    Took 18 minutes

--- packages/sanf/external/src/routes.php
+++ packages/sanf/external/src/routes.php
@@ -2,14 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'v1/external'], function () {
+Route::group(['prefix' => 'v1/external', 'middleware' => 'basic-auth-config:core-h2h-user-provider'], function () {
+    Route::post('push-notifications', ['as' => 'push-notifications.add', 'uses' => 'Notification\PushNotificationByExternalController@postAdd']);
+});
+
+Route::group(['prefix' => 'v1/public'], function () {
     Route::get('projects/{xid}/approve', ['as' => 'projects.approve', 'uses' => 'Project\ProjectByExternalController@postApproveByExternal']);
     Route::get('projects/{xid}/reject', ['as' => 'projects.reject', 'uses' => 'Project\ProjectByExternalController@postRejectByExternal']);
 
     Route::get('commodities/{xid}/approve', ['as' => 'commodities.approve', 'uses' => 'Commodity\CommodityByExternalController@postApproveByExternal']);
     Route::get('commodities/{xid}/reject', ['as' => 'commodities.reject', 'uses' => 'Commodity\CommodityByExternalController@postRejectByExternal']);
 
-    Route::group(['middleware' => 'basic-auth-config:core-h2h-user-provider'], function () {
-        Route::post('push-notifications', ['as' => 'push-notifications.add', 'uses' => 'Notification\PushNotificationByExternalController@postAdd']);
-    });
+    Route::post('push-notifications', ['as' => 'public.push-notifications.add', 'uses' => 'Notification\PushNotificationByExternalController@postAdd']);
 });
