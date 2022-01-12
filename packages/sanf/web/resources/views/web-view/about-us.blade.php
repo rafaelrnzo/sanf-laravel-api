0e753823c8295840e83f51dac27be686184aa297
Kevin Dwi Putra <kevin.dwiputra6@gmail.com>
2022-01-12 08:16:56 +0700

    [ADD] content for terms and condition, privacy policy and route for social media link at about us

--- packages/sanf/web/resources/views/web-view/about-us.blade.php
+++ packages/sanf/web/resources/views/web-view/about-us.blade.php
@@ -274,9 +274,9 @@
         <div class="mt-3">
           <p class="sf-title">Social Media Kami</p>
           <div class="sf-social-media mt-2">
-            <a href="#"><img src="{{ asset('assets/svg/web.svg') }}" alt="SANF Web" draggable="false" /></a>
-            <a href="#"><img src="{{ asset('assets/svg/ig.svg') }}" alt="SANF Instagram" draggable="false" /></a>
-            <a href="#"><img src="{{ asset('assets/svg/linkedin.svg') }}" alt="SANF Linkedin" draggable="false" /></a>
+            <a href="https://www.sanf.co.id" target="_blank"><img src="{{ asset('assets/svg/web.svg') }}" alt="SANF Web" draggable="false" /></a>
+            <a href="https://www.instagram.com/joinsanf/?hl=en" target="_blank"><img src="{{ asset('assets/svg/ig.svg') }}" alt="SANF Instagram" draggable="false" /></a>
+            <a href="https://id.linkedin.com/company/pt-surya-artha-nusantara-finance" target="_blank"><img src="{{ asset('assets/svg/linkedin.svg') }}" alt="SANF Linkedin" draggable="false" /></a>
           </div>
         </div>
         <div class="mt-3">
--- packages/sanf/web/resources/views/web-view/privacy-policy.php
+++ packages/sanf/web/resources/views/web-view/privacy-policy.php
@@ -64,6 +64,10 @@
       margin-bottom: 10px;
     }
 
+    .text-transform-none {
+      text-transform: none !important;
+    }
+
     .sf-text {
       font-family: 'Gilroy';
       font-style: normal;
@@ -73,6 +77,14 @@
       color: #999BAC;
       margin-bottom: 0;
       margin-top: 10px;
+    }
+
+    .sf-text-justify {
+      text-align: justify;
+    }
+
+    .sf-text-center {
+      text-align: center;
     }
 
     .sf-text-version {
@@ -187,16 +199,236 @@
   <div class="sf-wrapper">
     <div class="sf-container">
       <div class="sf-content">
-        <h3 class="sf-title">Kebijakan Privasi</h3>
-        <div>
-          <p class="sf-text">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
-            make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
-            Lorem Ipsum.
+        <h3 class="sf-title sf-text-center">Kebijakan Privasi Aplikasi SANFXtra</h3>
+        <div class="sf-text">
+          <p class="sf-text-justify">
+            Kebijakan Privasi berikut ini menjelaskan bagaimana PT Surya Artha Nusantara Finance selaku pemilik dan pengelola aplikasi 
+            seluler (<i>mobile application</i>) SANFXtra (“<strong>Aplikasi</strong>”) (selanjutnya disebut “<strong>SANF</strong>” atau 
+            “<strong>Kami</strong>”) memperoleh, mengumpulkan, menggunakan, menampilkan, mengumumkan, mengungkapkan, memproses, 
+            membukakan akses, menyimpan, mengirimkan, memberikan, membagikan, mengolah, mengelola, memusnahkan, melindungi, dan/atau 
+            melakukan kerjasama dengan pihak ketiga penyedia layanan teknologi informasi (secara bersama-sama, “<strong>Pemanfaatan</strong>”) 
+            data pribadi Pengguna yang meliputi antara lain, namun tidak terbatas pada data-data yang terdapat dalam kartu identitas Pengguna, 
+            nomor telepon dan informasi lainnya (“<strong>Data Pribadi</strong>”) yang Pengguna berikan ketika Pengguna menggunakan Aplikasi 
+            Kami, serta data-data yang terkait dengan transaksi yang Pengguna lakukan dengan menggunakan Aplikasi 
+            (“<strong>Data Transaksi</strong>”). Perlu dicatat bahwa Data Pribadi disini tidak termasuk Data Pribadi yang telah tersedia 
+            di publik.
           </p>
+          <p class="sf-text-justify">
+            Kebijakan Privasi ini merupakan bagian dari Syarat dan Ketentuan Penggunaan Aplikasi (“Syarat & Ketentuan”) Kami. Penggunaan Aplikasi 
+            dan setiap fitur dan/atau layanan yang tersedia dalam Aplikasi merupakan bentuk persetujuan Pengguna terhadap ketentuan dan Kebijakan 
+            Privasi ini. Oleh karena itu, mohon Pengguna membaca Kebijakan Privasi ini dengan seksama untuk memastikan bahwa Pengguna memahami 
+            sepenuhnya sebelum mendaftar, mengakses dan/atau menggunakan Aplikasi.
+          </p>
+
+          <div class="mt-3">
+
+            <h3 class="sf-title">Informasi Pribadi Pengguna Aplikasi</h3>
+            <h3 class="sf-title text-transform-none">Pengumpulan Data Pribadi dan/atau Data Transaksi</h3>
+            <p class="sf-text-justify">
+              Data Pribadi dapat Pengguna berikan secara langsung pada saat Pengguna mendaftarkan diri dalam Aplikasi Kami maupun pada saat 
+              menggunakan layanan. Data Transaksi diperoleh setiap saat Pengguna menggunakan layanan yang tersedia dalam Aplikasi.
+            </p>
+            <p class="sf-text-justify">
+              Kami mengumpulkan informasi dari berbagai sumber sesuai dengan peraturan perundang-undangan yang berlaku, baik data baru maupun 
+              data historis, agar Kami dapat selalu memberikan layanan terbaik kepada pengguna Aplikasi dari waktu ke waktu. Pengguna dengan 
+              ini setuju dan memberikan wewenang kepada Kami untuk (i) melakukan Pemanfaatan atas Data Pribadi dan/atau Data Transaksi yang 
+              Pengguna berikan kepada Kami; (ii) mengungkapkan, memberikan, mengirimkan dan membagikan Data Pribadi dan/atau Data Transaksi 
+              Pengguna kepada afiliasi Kami sepanjang memenuhi peraturan perundang-undangan yang berlaku; (iii) mengumpulkan informasi lainnya 
+              secara tidak langsung meliputi akses ke kamera, tanggal saat pembuatan Akun Aplikasi, tanggal submit pengajuan, akses ke GPS, 
+              serta informasi lainnya sesuai dengan interaksi Pengguna dalam Aplikasi kami. Pemberian Data Pribadi Pengguna bersifat sukarela, 
+              namun jika Pengguna tidak memberikan Data Pribadi Pengguna kepada Kami dengan benar atau menarik setiap atau seluruh informasi 
+              yang termasuk ke dalam Data Pribadi, maka Kami mungkin tidak dapat memberikan layanan kepada Pengguna sebagaimana mestinya.
+            </p>
+  
+            <h3 class="sf-title text-transform-none">Penggunaan Data Pribadi dan/atau Data Transaksi</h3>
+            <ol>
+              <li>
+                <p class="sf-text-justify">
+                  Pengguna dengan ini memberikan persetujuan secara tegas kepada Kami untuk melakukan Pemanfaatan Data Pribadi dan/atau 
+                  Data Transaksi Pengguna untuk penggunaaan Aplikasi, kegiatan bisnis dan operasional Kami maupun tujuan-tujuan lain 
+                  sebagai berikut:
+                </p>
+                <ul>
+                  <li>
+                    <p class="sf-text-justify">Menanggapi pertanyaan, komentar atau keluhan;</p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Berkomunikasi dengan Pengguna melalui berbagai media seperti notifikasi dalam Aplikasi, surat elektronik, telepon, 
+                      layanan pesan singkat (SMS), layanan percakapan (chatting) pesan singkat dalam Aplikasi 
+                      (in-app messaging/ push notifications), maupun media sosial;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Pemasaran dan promosi produk/layanan Kami dan/atau perusahaan afiliasi Kami, termasuk namun tidak terbatas pada pengumuman, 
+                      materi promosi, ucapan salam, undangan untuk berpartisipasi dan hak-hak istimewa;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Memproses partisipasi Pengguna dalam setiap kegiatan produksi, kontes, permainan, promosi, jajak pendapat maupun survei;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Memahami dan menganalisa data dan/atau pola penggunaan dan penjualan produk dan layanan terkait kebutuhan dan preferensi;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Melakukan kegiatan pengumpulan dan pengolahan data, termasuk dengan interaksi Pengguna dengan Aplikasi untuk meningkatkan 
+                      kualitas layanan;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Mengembangkan, meningkatkan dan menyediakan produk dan layanan yang sesuai dengan kebutuhan Pengguna;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Proses administrasi internal;
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Setiap tujuan lain yang Kami beritahukan kepada Pengguna pada saat mendapatkan persetujuan Pengguna; dan/atau
+                    </p>
+                  </li>
+                  <li>
+                    <p class="sf-text-justify">
+                      Keperluan lainnya sesuai dengan peraturan perundang-undangan yang berlaku.
+                    </p>
+                  </li>
+                </ul>
+  
+                (Secara bersama-sama, tujuan-tujuan di atas merupakan "<strong>Tujuan</strong>").
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Kami dengan ini menjamin bahwa Kami akan menggunakan Data Pribadi Pengguna hanya sejauh diperlukan sebagaimana disepakati 
+                  oleh Pengguna yakni kami hanya memproses Data Pribadi dalam ruang lingkup yang diperlukan untuk mencapai Tujuan seperti yang 
+                  dijelaskan diatas.
+                </p>
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Kami berupaya secara wajar untuk melindungi setiap Data Pribadi dan/atau Data Transaksi Pengguna. Kami senantiasa berusaha 
+                  meningkatkan standar perlindungan Aplikasi. Kami tidak akan membuka Data Pribadi Pengguna kepada Pihak yang tidak memiliki 
+                  wewenang atas informasi yang Pengguna berikan. Namun demikian, Kami tidak bertanggung jawab atas setiap kerugian yang timbul 
+                  akibat rusaknya atau hilangnya Data Pribadi dan/atau Data Transaksi Pengguna yang terjadi diluar kendali Kami. Oleh karena itu, 
+                  Kami sangat menyarankan agar Pengguna menjaga keamanan dan kerahasiaan Akun Aplikasi Pengguna, termasuk alamat surat elektronik 
+                  terdaftar, nomor telepon terdaftar, dan kode OTP (<i>One Time Password</i>) yang dihasilkan dan dikirim oleh sistem Kami.
+                </p>
+              </li>
+            </ol>
+  
+            <h3 class="sf-title text-transform-none">Pemberian dan Pengungkapan Data Pribadi dan/atau Data Transaksi</h3>
+            <p class="sf-text-justify">
+              Dengan tunduk kepada Tujuan sebagaimana disebutkan di atas, Pengguna dengan ini memberikan persetujuan secara tegas kepada Kami 
+              untuk mengungkapkan Data Pribadi dan/atau Data Transaksi kepada pihak ketiga dengan ketentuan di bawah ini:
+            </p>
+            <ul>
+              <li>
+                <p class="sf-text-justify">
+                  Perusahaan riset dan pemasaran terkait promosi produk/layanan Kami yang sudah memiliki perjanjian sebelumnya;
+                </p>
+              </li>
+              <li>
+                <p class="sf-text-justify">Perusahaan afiliasi Kami sesuai dengan kebutuhannya;</p>
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Perusahaan penyedia layanan terkait teknologi informasi (termasuk layanan infrastruktur, komputasi awan (<i>cloud computing</i>), 
+                  perangkat lunak, analisa big data dan <i>machine learning</i>, termasuk di dalamnya layanan <i>data cleansing, data insight</i> 
+                  dan <i>credit scoring</i>);
+                </p>
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Perusahaan penyedia layanan lainnya yang terkait dengan produk dan layanan yang ditawarkan dalam Aplikasi;
+                </p>
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Penasihat profesional dan auditor eksternal, termasuk penasihat hukum, penasihat keuangan dan konsultan lainnya;
+                </p>
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Otoritas pemerintah, baik di dalam maupun di luar yurisdiksi negara Republik Indonesia sesuai peraturan perundang-undangan 
+                  yang berlaku atau jika secara hukum Kami harus melakukannya; dan/atau
+                </p>
+              </li>
+              <li>
+                <p class="sf-text-justify">
+                  Pihak-pihak lainnya sesuai dengan peraturan perundang-undangan yang berlaku.
+                </p>
+              </li>
+            </ul>
+
+            <h3 class="sf-title text-transform-none">Pengelolaan dan Keamanan Data Pribadi dan/atau Data Transaksi</h3>
+            <p class="sf-text-justify">
+              Pengguna dengan ini memberikan persetujuan secara tegas kepada Kami bahwa Kami dapat mengirimkan, menyimpan, menggunakan dan 
+              mengolah Data Pribadi dan/atau Data Transaksi Pengguna pada server yang terletak di pusat data yang ditunjuk oleh Kami. Pusat 
+              data tersebut dapat dikelola oleh pihak ketiga sesuai dengan ketentuan peraturan perundang-undangan yang berlaku. Meskipun 
+              demikian, Pemanfaatan Data Pribadi dan/atau Data Transaksi Pengguna sehubungan dengan penggunaan Aplikasi akan terus diatur oleh 
+              Kebijakan Privasi ini sesuai dengan peraturan perundangan-undangan yang berlaku.
+            </p>
+            <p class="sf-text-justify">
+              Kami berusaha untuk memelihara pengamanan fisik, teknis dan prosedural yang sesuai untuk melindungi Data Pribadi Pengguna dari 
+              kehilangan, penyalahgunaan, penyalinan, kerusakan atau modifikasi dan akses atau pengungkapan yang tidak sah.
+            </p>
+            <p class="sf-text-justify">
+              Kami akan menyimpan Data Pribadi dan/atau Data Transaksi Pengguna hingga ada permintaan khusus untuk menghapus Akun Aplikasi 
+              Pengguna. Lebih lanjut, Kami tidak akan bertanggung jawab atas pelanggaran keamanan atau tindakan apapun dari pihak ketiga atau 
+              peristiwa apapun yang berada diluar kendali wajar Kami termasuk namun tidak terbatas pada tindakan pemerintah, peretasan komputer, 
+              akses tidak sah ke data komputer dan perangkat penyimpanan, komputer crash, pelanggaran keamanan dan enkripsi, buruknya kualitas 
+              layanan Internet atau layanan telepon dari Pengguna, dan lain-lain.
+            </p>
+
+            <h3 class="sf-title text-transform-none">Pembaruan dan Pengubahan Data Pribadi</h3>
+            <p class="sf-text-justify">
+              Pengguna dapat mengubah Data Pribadi di Profil Pengguna sewaktu-waktu. Pengguna setuju bahwa perubahan tersebut dapat mempengaruhi 
+              akses terhadap Aplikasi dan/atau layanan yang dapat Pengguna pergunakan. Pengguna dapat menghubungi Kami melalui surat elektronik 
+              layanan.konsumen@sanf.co.id untuk memberitahu Kami terkait hal tersebut. Harap perhatikan juga bahwa menghapus instalasi Aplikasi 
+              tidak akan mengakibatkan penghentian penggunaan layanan oleh Pengguna atau Pemanfaatan Data Pribadi Pengguna.
+            </p>
+
+            <h3 class="sf-title text-transform-none">Perjanjian Pengguna dan Perubahan Pada Kebijakan Privasi</h3>
+            <p class="sf-text-justify">
+              Dengan mendaftar, mengakses dan/atau menggunakan Aplikasi, Pengguna dianggap telah membaca, memahami dan memberikan persetujuan 
+              secara tegas atas Kebijakan Privasi Kami sebagaimana dijelaskan di atas. Pengguna juga menyatakan dan menjamin bahwa Data Pribadi 
+              yang Pengguna berikan kepada Kami adalah benar, lengkap, diperoleh secara sah dan Pengguna memiliki hak sepenuhnya untuk 
+              memberikannya kepada Kami.
+            </p>
+            <p class="sf-text-justify">
+              Kami dapat meninjau, memperbarui, atau mengubah Kebijakan Privasi ini sewaktu-waktu dengan memunculkan informasi atas perubahan 
+              tersebut pada Aplikasi. Akses dan/atau penggunaan Aplikasi secara terus-menerus serta tidak adanya pemberitahuan secara tertulis 
+              kepada Kami mengenai keberatan Pengguna atas perubahan yang dibuat merupakan bentuk penerimaan dan persetujuan Pengguna atas 
+              perubahan Kebijakan Privasi tersebut.
+            </p>
+            <p class="sf-text-justify">
+              Ini adalah tanggung jawab Pengguna untuk secara berkala meninjau Kebijakan Privasi ini dan untuk mengetahui informasi terkini 
+              tentang bagaimana ketentuan Kebijakan Privasi ini Kami berlakukan. Pengguna bertanggung jawab untuk senantiasa memahami perubahan 
+              yang Kami buat dari waktu ke waktu. Kebijakan Privasi mulai berlaku sejak tanggal pembaruan, perubahan atau modifikasi tersebut.
+            </p>
+
+            <h3 class="sf-title text-transform-none">Lain-lain</h3>
+            <p class="sf-text-justify">
+              Kebijakan Privasi ini diatur dan ditafsirkan menurut hukum yang berlaku di negara Republik Indonesia dan setiap sengketa yang 
+              timbul dari Kebijakan Privasi ini akan diselesaikan melalui Pengadilan Negeri Jakarta Selatan.
+            </p>
+
+            <h3 class="sf-title text-transform-none">Kontak Kami</h3>
+            <p class="sf-text-justify">
+              Pengguna memiliki hak atas perbaikan dan penghapusan Data Pribadi Pengguna termasuk informasi lebih lanjut mengenai penggunaan 
+              Aplikasi dan transaksi. Apabila Pengguna membutuhkan informasi lebih lanjut maupun terdapat keluhan sehubungan dengan hal ini, 
+              Pengguna dapat menghubungi Kami melalui surat elektronik layanan.konsumen@sanf.co.id.
+            </p>
+          </div>
+        
         </div>
       </div>
     </div>
