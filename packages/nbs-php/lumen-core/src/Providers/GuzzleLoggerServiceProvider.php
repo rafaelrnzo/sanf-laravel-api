da075daf9a925e33c57252df0ba612b4ef107cd2
gozals25 <gozals25@nusantarabetastudio.com>
2022-03-18 10:41:44 +0700

    fix: guzzle logger track request body
    Took 19 minutes

--- packages/nbs-php/lumen-core/src/Providers/GuzzleLoggerServiceProvider.php
+++ packages/nbs-php/lumen-core/src/Providers/GuzzleLoggerServiceProvider.php
@@ -80,10 +80,12 @@ class GuzzleLoggerServiceProvider extends ServiceProvider
             if ($isEnabled && $logDriver == self::DRIVER_DATABASE) {
                 $handlerStack->push(
                     Middleware::tap(null, function (RequestInterface $request, array $options, PromiseInterface $promise) {
-                        $promise->then(function (ResponseInterface $response) use ($request) {
+                        $requestBody = $request->getBody();
+                        $requestBody->rewind();  // need to rewind stream to be able read request content again
+                        $promise->then(function (ResponseInterface $response) use ($request, $requestBody) {
                             return dispatch(new LogApiRequestToDatabaseJob(
                                 $request,
-                                $request->getBody()->getContents(),
+                                $requestBody->getContents(),
                                 $response,
                                 $response->getBody()->getContents(),
                             ));
