494fd6f73ae431f728bf7dab50d69ba85cec0c9e
nbs-diar <diar@nusantarabetastudio.com>
2022-07-08 09:41:57 +0700

    refactor(e-sign): Add location's route by tekenAja

--- packages/sanf/integration/src/tekenaja-routes.php
+++ packages/sanf/integration/src/tekenaja-routes.php
@@ -4,7 +4,7 @@ use NbsPhp\ApiWrapper\Api\Route;
 use Sanf\Integration\TekenAjaInternalApiProcessor;
 
 Route::group(config('tekenaja-internal.url'), [TekenAjaInternalApiProcessor::class], function () {
-    Route::get('location.province', 'v2/data/province');
-    Route::get('location.district', 'v2/data/district');
-    Route::get('location.subdistrict', 'v2/data/subdistrict');
+    Route::get('location.province', '/v2/data/province');
+    Route::get('location.district', '/v2/data/district');
+    Route::get('location.subdistrict', '/v2/data/subdistrict');
 });
