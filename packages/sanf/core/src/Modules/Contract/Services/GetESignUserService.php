8a103f7170919341b322944fc51f3f3de8095eb3
nbs-diar <diar@nusantarabetatstudio.com>
2022-07-19 15:38:59 +0700

    fix(e-sign): Return Status ID of User TekenAja

--- packages/sanf/core/src/Modules/Contract/Services/GetESignUserService.php
+++ packages/sanf/core/src/Modules/Contract/Services/GetESignUserService.php
@@ -59,7 +59,6 @@ final class GetESignUserService implements ApplicationServiceInterface
                 'gender' => isset($item['GENDER']) ? (int) $item['GENDER'] : null,
                 'address' => isset($item['ADDRESS']) ? $item['ADDRESS'] : null,
                 'postalCode' => isset($item['ZIP_CODE']) ? (int) $item['ZIP_CODE'] : null,
-                'statusId' => ESignRegistrationStatusEnum::AVAILABLE,
             ];
         }, $response['data'])[0];
 
@@ -73,6 +72,7 @@ final class GetESignUserService implements ApplicationServiceInterface
         if ($resultTekenAja['code'] and !$exceptCodeCondition) {
             $this->errorHandle($resultTekenAja['code'], $resultTekenAja['message'], $result['email'], $result['nik']);
         } else {
+            $result['statusId'] = $userTekenAja->status_id;
             if (!$userTekenAja) {
                 $request['xid'] = nano_id();
                 $request['email'] = $result['email'];
