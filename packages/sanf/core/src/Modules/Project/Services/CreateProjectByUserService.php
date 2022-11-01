9d29608b939dc2f68054b72aea2aeb542461e08b
nbs-diar <diar@nusantarabetatstudio.com>
2022-11-01 10:47:30 +0700

    refactor(project): provide null image when add project

--- packages/sanf/core/src/Modules/Project/Services/CreateProjectByUserService.php
+++ packages/sanf/core/src/Modules/Project/Services/CreateProjectByUserService.php
@@ -67,7 +67,7 @@ class CreateProjectByUserService extends ProjectByUserService implements Applica
 //            'modified_by' => //TODO USER SNAPSHOT
             'city_name' => $dto->locationMetadata['city_name'],
             'province_name' => $dto->locationMetadata['province_name'],
-            'image_path' => $imageFile['path']
+            'image_path' => $imageFile['path'] ?? null,
         ]);
 
         event(new ProjectCreatedEvent($project));
