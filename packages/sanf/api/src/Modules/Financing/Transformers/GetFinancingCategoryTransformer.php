576c95cd2f4fb5cb7f8d51d5d1c936557ddea49c
nbs-diar <diar@nusantarabetatstudio.com>
2022-11-23 17:44:15 +0700

    refactor(financing-application-category): add button value and icon url

--- packages/sanf/api/src/Modules/Financing/Transformers/GetFinancingCategoryTransformer.php
+++ packages/sanf/api/src/Modules/Financing/Transformers/GetFinancingCategoryTransformer.php
@@ -12,9 +12,11 @@ class GetFinancingCategoryTransformer extends TransformerAbstract
         return [
             'xid' => $item->xid,
             'title' => $item->title,
+            'icon_url' => $item->icon_url,
             'image_url' => $item->image_url,
             'description' => $item->description,
             'button_label' => $item->button_label,
+            'button_value' => $item->button_value,
             'created_at' => unix_timestamp($item->created_at),
         ];
     }
--- packages/sanf/core/database/migrations/2022_11_21_034406_create_financing_category_table.php
+++ packages/sanf/core/database/migrations/2022_11_21_034406_create_financing_category_table.php
@@ -19,13 +19,15 @@ class CreateFinancingCategoryTable extends Migration
             $table->string('xid', 32)->index()->unique();
             $table->string('title')->nullable();
             $table->longText('description')->nullable();
+            $table->string('icon_url')->nullable();
             $table->string('image_url')->nullable();
             $table->string('button_label')->nullable();
+            $table->string('button_value')->nullable();
             $table->timestamp('created_at');
         });
 
         DB::table('financing_category')->insertOrIgnore([
-            ['id' => 1, 'xid' => 'jtl-CKbzYXXYollyRGekv', 'title' => 'Lorem ipsum', 'description' => '<h2>Lorem ipsum..</h2>', 'image_url' => 'https://via.placeholder.com/400x400.png?text=Image', 'button_label' => 'Lorem Ipsum', 'created_at' => '2022-01-01 00:00:00',],
+            ['id' => 1, 'xid' => 'jtl-CKbzYXXYollyRGekv', 'title' => 'Lorem ipsum', 'description' => '<h2>Lorem ipsum..</h2>', 'icon_url' => 'https://via.placeholder.com/100x100.png?text=Icon', 'image_url' => 'https://via.placeholder.com/400x400.png?text=Image', 'button_label' => 'Lorem Ipsum', 'button_value' => 'financingMethodId', 'created_at' => '2022-01-01 00:00:00',],
         ]);
     }
 
