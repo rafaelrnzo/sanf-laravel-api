7e339a306ec6862808e377de8523180577dfdd4b
nbs-diar <diar@nusantarabetatstudio.com>
2022-11-29 16:54:18 +0700

    refactor(financing-application): compose attachment for simulation

--- packages/sanf/core/src/Modules/Financing/Services/GetPdfFinancingSimulationService.php
+++ packages/sanf/core/src/Modules/Financing/Services/GetPdfFinancingSimulationService.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace Sanf\Core\Modules\Financing\Services;
 
 use Dompdf\Dompdf;
 use NbsPhp\Core\Services\ApplicationServiceInterface;
 use Sanf\Core\Modules\Financing\Dto\PdfFinancingSimulationRequestDto;
 use Sanf\Core\Modules\Financing\Enums\FinancingMethodEnum;
-
 
 class GetPdfFinancingSimulationService extends FinancingByUserService implements ApplicationServiceInterface
 {
@@ -19,40 +17,53 @@ class GetPdfFinancingSimulationService extends FinancingByUserService implements
     public function execute($dto = null)
     {
         $user = $this->findUserOrFail($dto->user_id);
-        $isAnjak = in_array($dto->financing_method_id, [
-            FinancingMethodEnum::ANJAK_PIUTANG_PEMBERIAN,
-            FinancingMethodEnum::ANJAK_PIUTANG_TANPA_PEMBERIAN,
-        ]);
-        if ($dto->financing_method_id == FinancingMethodEnum::FASILITAS_MODAL_USAHA) {
-            $bodyContent = [
-                'cara_pembiayaan' => $dto->financing_method_name,
-                'nilai_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount, 0, ',', '.'),
-                'tenor' => $dto->tenor_in_month . ' Bulan',
-                'suku_bunga' => $dto->interest_rate_percentage . '%',
-                'angsuran_per_bulan' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
-            ];
-        } else if ($isAnjak) {
-            $bodyContent = [
+        switch ($dto->financing_method_id) {
+            case FinancingMethodEnum::PEMBELIAN_ANGSURAN:
+                $bodyContent = [
+                    'cara_pembiayaan' => $dto->financing_method_name,
+                    'harga_unit_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount, 0, ',', '.'),
+                    '%_uang_muka_(%_dp)' => $dto->down_payment_percentage . '%',
+                    'uang_muka_(dp)' => 'Rp. ' . number_format($dto->down_payment_amount, 0, ',', '.'),
+                    'tenor' => $dto->tenor_in_month . ' Bulan',
+                    'nilai_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount - $dto->down_payment_amount, 0, ',', '.'),
+                    'suku_bunga' => $dto->interest_rate_percentage . '%',
+                    'angsuran_perbulan' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
+                ];
+                break;
+            case FinancingMethodEnum::FASILITAS_MODAL_USAHA:
+                $bodyContent = [
+                    'cara_pembiayaan' => $dto->financing_method_name,
+                    'nilai_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount, 0, ',', '.'),
+                    'tenor' => $dto->tenor_in_month . ' Bulan',
+                    'suku_bunga' => $dto->interest_rate_percentage . '%',
+                    'angsuran_per_bulan' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
+                ];
+                break;
+            case FinancingMethodEnum::ANJAK_PIUTANG_PEMBERIAN:
+            case FinancingMethodEnum::ANJAK_PIUTANG_TANPA_PEMBERIAN:
+                $bodyContent = [
                 'cara_pembiayaan' => $dto->financing_method_name,
                 'total_invoice' => 'Rp. ' . number_format($dto->financing_amount, 0, ',', '.'),
                 '%_retensi' => $dto->down_payment_percentage . '%',
                 'nilai_retensi' => 'Rp. ' . number_format($dto->down_payment_amount, 0, ',', '.'),
                 'tenor' => $dto->tenor_in_month . ' Bulan',
-                'nilai_anjak_piutang' => 'Rp. ' . number_format($dto->financing_amount - $dto->down_payment_amount, 0, ',', '.'),
-                'diskonto' => $dto->interest_rate_percentage . '%',
-                'nilai_angsuran' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
-            ];
-        } else {
-            $bodyContent = [
-                'cara_pembiayaan' => $dto->financing_method_name,
-                'harga_unit_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount, 0, ',', '.'),
-                '%_uang_muka_(%_dp)' => $dto->down_payment_percentage . '%',
-                'uang_muka(dp)' => 'Rp. ' . number_format($dto->down_payment_amount, 0, ',', '.'),
-                'tenor' => $dto->tenor_in_month . ' Bulan',
                 'nilai_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount - $dto->down_payment_amount, 0, ',', '.'),
-                'suku_bunga' => $dto->interest_rate_percentage . '%',
-                'angsuran_perbulan' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
-            ];
+                'diskonto' => $dto->interest_rate_percentage . '%',
+                'total_pencairan' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
+                ];
+                break;
+            default:
+                $bodyContent = [
+                    'cara_pembiayaan' => $dto->financing_method_name,
+                    'harga_unit_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount, 0, ',', '.'),
+                    '%_security_deposit_(%_sd)' => $dto->down_payment_percentage . '%',
+                    'security_deposit_(sd)' => 'Rp. ' . number_format($dto->down_payment_amount, 0, ',', '.'),
+                    'tenor' => $dto->tenor_in_month . ' Bulan',
+                    'nilai_pembiayaan' => 'Rp. ' . number_format($dto->financing_amount - $dto->down_payment_amount, 0, ',', '.'),
+                    'suku_bunga' => $dto->interest_rate_percentage . '%',
+                    'angsuran_perbulan' => 'Rp. ' . number_format($dto->installment_per_month, 0, ',', '.'),
+                ];
+                break;
         }
 
         $template = view('vendor/pdf/PDFView', ['contents' => [
@@ -60,7 +71,7 @@ class GetPdfFinancingSimulationService extends FinancingByUserService implements
                 0 => $this->toBase64(asset('assets/png/sanf-logo-blue.png')),
                 1 => $this->toBase64(asset('assets/png/sanf-tagline.png')),
             ],
-            'openingSentence' => 'Selamat siang '. $user->full_name. ', berikut kami lampirkan hasil perhitungan simulasi pengajuan pembiayaan anda',
+            'openingSentence' => 'Selamat siang ' . $user->full_name . ', berikut kami lampirkan hasil perhitungan simulasi pengajuan pembiayaan anda',
             'email' => $user->username,
             'data' => $bodyContent,
             'closingSentence' => 'Hasil perhitungan ini bersifat tidak mengikat',
