a5523b73f872222fafee25b01bc843251c3cdd6b
nbs-diar <diar@nusantarabetatstudio.com>
2023-01-26 17:04:31 +0700

    build: Add dockerfile for api

---
+++ deployments/v2/app/api/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo '  > '
+if [ ! -d "/etc/supervisor/conf.d" ]; then
+    echo '  > WARN: /etc/supervisor/conf.d not found'
+    echo '  > '
+    echo '  > Creating a supervisor conf.d directory...'
+    echo '  > mkdir /etc/supervisor/conf.d'
+    mkdir /etc/supervisor/conf.d
+    echo '  > mkdir: "/etc/supervisor/conf.d": Is a directory'
+fi
+
+echo '  > '
+echo '  > Autoload class...'
+composer dump
+
+echo '  > '
+echo '  > Running migration...'
+php artisan migrate
+
+echo '  > '
+echo '  > Symlinking supervisor configuration...'
+ln -sf /etc/supervisor/conf.d-available/app.conf /etc/supervisor/conf.d/app.conf
+
+echo '  > '
+echo '  > Execute supervisord...'
+exec supervisord -c /etc/supervisor/supervisord.conf
