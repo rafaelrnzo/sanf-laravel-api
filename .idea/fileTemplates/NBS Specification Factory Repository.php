8bd70f8ed6c29b1f08b3c216bb0f1e0b131e3bb4
nbs-diar <diar@nusantarabetastudio.com>
2023-04-12 10:22:10 +0700

    chore: Remove .idea

--- .idea/fileTemplates/NBS Specification Factory Repository.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace ${VENDOR}\Core\Modules\\${MODULE}\Specifications;
-
-interface ${NAME}SpecificationFactoryInterface
-{
-    public function paginate(?int \$skip, ?int \$limit, ?string \$sortBy, ?string \$keyword);
-}
