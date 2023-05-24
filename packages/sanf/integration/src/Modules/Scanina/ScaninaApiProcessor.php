d39e4d71f32b68698b9f1742c5af2f86ab59dd07
nbs-diar <diar@nusantarabetastudio.com>
2023-05-24 11:42:23 +0700

    refactor(integration): Add header authorization at scanina api

--- packages/sanf/integration/src/Modules/Scanina/ScaninaApiProcessor.php
+++ packages/sanf/integration/src/Modules/Scanina/ScaninaApiProcessor.php
@@ -6,26 +6,19 @@ use GuzzleHttp\Exception\ServerException;
 use NbsPhp\ApiWrapper\Api\Processor;
 use NbsPhp\ApiWrapper\Api\Request;
 use NbsPhp\ApiWrapper\Api\Response;
-use Sanf\Integration\Exceptions\TekenAjaExternalApiDataNotFoundException;
-use Sanf\Integration\Exceptions\TekenAjaExternalApiException;
 
 class ScaninaApiProcessor extends Processor
 {
     public static function handle(Request $request, callable $next): Response
     {
-        $request->headers(['Client-id' => config('scanina.client_id')]);
+        $base64 = base64_encode(config('scanina-api.client_id') . ":" . config('scanina-api.client_secret'));
+        $request->headers(['Authorization' => "Basic {$base64}"]);
         $request->headers(['X-Request-ID' => app('request')->header('X-Request-ID')]);
         try {
             $response = $next($request);
             $result = $response->json();
             if (is_null($result)) {
                 throw new \Exception('Something went wrong at SCANINA API');
-            }
-            if ($result['status'] === false) {
-                if ($result['code'] === 'E_EmptyData') {
-                    throw new TekenAjaExternalApiDataNotFoundException($result['message']);
-                }
-                throw new TekenAjaExternalApiException($result['message']);
             }
         } catch (ServerException $exception) {
             //TODO HANDLE EXCEPTION
