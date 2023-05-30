a96ca7a9e44f5f4fb1945498285bd9e08076928b
nbs-diar <diar@nusantarabetastudio.com>
2023-05-30 10:27:38 +0700

    refactor(scanina): Update integration route path

--- packages/sanf/integration/src/Modules/Scanina/scanina-routes.php
+++ packages/sanf/integration/src/Modules/Scanina/scanina-routes.php
@@ -4,25 +4,30 @@ use NbsPhp\ApiWrapper\Api\Route;
 use Sanf\Integration\Modules\Scanina\ScaninaApiProcessor;
 
 Route::group(config('scanina-api.url'), [ScaninaApiProcessor::class], function () {
-    Route::get('scanina.product.buy.browse', '/v1/buy/list-products');
-    Route::get('scanina.product.buy.read', '/v1/buy/detail-product/{xid}');
-    Route::get('scanina.product.buy.category.browse', '/v1/buy/list-categories');
-    Route::get('scanina.product.buy.brand.browse', '/v1/buy/list-brand');
-    Route::get('scanina.product.buy.type.browse', '/v1/buy/list-types');
-    Route::get('scanina.product.buy.model.browse', '/v1/buy/list-models');
+    Route::get('scanina.product.buy.browse', '/v1/product/buy');
+    Route::get('scanina.product.buy.read', '/v1/product/buy/{xid}');
+    Route::get('scanina.product.buy.category.browse', '/v1/category/buy');
+    Route::get('scanina.product.buy.brand.browse', '/v1/brand/buy');
+    Route::get('scanina.product.buy.type.browse', '/v1/type/buy');
+    Route::get('scanina.product.buy.model.browse', '/v1/model/buy');
 
-    Route::get('scanina.product.rent.browse', '/v1/rental/list-products');
-    Route::get('scanina.product.rent.category.browse', '/v1/rental/list-categories');
-    Route::get('scanina.product.rent.brand.browse', '/v1/rental/list-brand');
-    Route::get('scanina.product.rent.type.browse', '/v1/rental/list-types');
-    Route::get('scanina.product.rent.model.browse', '/v1/rental/list-models');
+    Route::get('scanina.product.rent.browse', '/v1/product/rental');
+    Route::get('scanina.product.rent.read', '/v1/product/rental/{xid}');
+    Route::get('scanina.product.rent.category.browse', '/v1/category/rental');
+    Route::get('scanina.product.rent.brand.browse', '/v1/brand/rental');
+    Route::get('scanina.product.rent.type.browse', '/v1/type/rental');
+    Route::get('scanina.product.rent.model.browse', '/v1/model/rental');
 
-    Route::get('scanina.product.service.browse', '/v1/service/list-products');
-    Route::get('scanina.product.service.category.browse', '/v1/service/list-categories');
 
-    Route::get('scanina.product.spare-part.browse', '/v1/spare-part/list-products');
-    Route::get('scanina.product.spare-part.category.browse', '/v1/spare-part/list-categories');
-    Route::get('scanina.product.spare-part.brand.browse', '/v1/spare-part/list-brands');
+    Route::get('scanina.product.service.browse', '/v1/product/service');
+    Route::get('scanina.product.service.read', '/v1/product/service/{xid}');
+    Route::get('scanina.product.service.category.browse', '/v1/category/service');
+
+    Route::get('scanina.product.spare-part.browse', '/v1/product/spare-part');
+    Route::get('scanina.product.spare-part.read', '/v1/product/spare-part/{xid}');
+    Route::get('scanina.product.spare-part.category.browse', '/v1/category/spare-part');
+    Route::get('scanina.product.spare-part.brand.browse', '/v1/brand/spare-part');
+
 
     Route::get('scanina.city.browse', '/v1/list-cities');
 });
