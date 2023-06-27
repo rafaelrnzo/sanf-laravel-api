db93bc0679e986e9d7ffe0fb95864e7a3f7fb500
nbs-diar <diar@nusantarabetastudio.com>
2023-06-27 16:43:35 +0700

    feat(scanina): Add code response for user not registered

--- packages/sanf/integration/src/Modules/Scanina/ScaninaApiProcessor.php
+++ packages/sanf/integration/src/Modules/Scanina/ScaninaApiProcessor.php
@@ -10,9 +10,13 @@ use NbsPhp\ApiWrapper\Api\Response;
 use NbsPhp\Core\Enum\HttpStatusCode;
 use Sanf\Core\Modules\Scanina\Exceptions\ScaninaProductInvalidRequestException;
 use Sanf\Core\Modules\Scanina\Exceptions\ScaninaProductNotFoundException;
+use Sanf\Core\Modules\Scanina\Exceptions\ScaninaUserNotRegisteredException;
 
 class ScaninaApiProcessor extends Processor
 {
+    // TODO use enum class
+    public const NOT_REGISTERED = 'E_ACCOUNT_2';
+
     public static function handle(Request $request, callable $next): Response
     {
         $base64 = base64_encode(config('scanina-api.client_id') . ":" . config('scanina-api.client_secret'));
@@ -25,10 +29,15 @@ class ScaninaApiProcessor extends Processor
                 throw new Exception('Something went wrong at SCANINA API');
             }
         } catch (ClientException $exception) {
-            $code = $exception->getCode();
+            $response = $exception->getResponse();
+            $bodyString = $response->getBody()->getContents();
+            $body = is_string($bodyString) ? json_decode($bodyString) : $bodyString;
+            $code = $body->code ?? $exception->getCode();
+
             $exceptions = [
                 HttpStatusCode::HTTP_NOT_FOUND => new ScaninaProductNotFoundException(),
                 HttpStatusCode::HTTP_BAD_REQUEST => new ScaninaProductInvalidRequestException(),
+                self::NOT_REGISTERED => new ScaninaUserNotRegisteredException(),
             ];
 
             if (!array_key_exists($code, $exceptions)) {
