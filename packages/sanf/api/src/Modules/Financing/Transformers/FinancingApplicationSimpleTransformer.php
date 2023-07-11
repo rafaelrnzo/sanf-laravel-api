45c57b96ee7bcce9c9b11b0734e764e9ae9f4cf3
nbs-diar <diar@nusantarabetastudio.com>
2023-07-11 14:09:33 +0700

    fix(financing-application): Set type data for consistency

--- packages/sanf/api/src/Modules/Financing/Transformers/FinancingApplicationSimpleTransformer.php
+++ packages/sanf/api/src/Modules/Financing/Transformers/FinancingApplicationSimpleTransformer.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Sanf\Api\Modules\Financing\Transformers;
-
 
 use League\Fractal\TransformerAbstract;
 
@@ -11,15 +9,15 @@ class FinancingApplicationSimpleTransformer extends TransformerAbstract
     public function transform($item)
     {
         return [
-            'xid' => $item->xid,
-            'application_code' => $item->application_code,
+            'xid' => (string)$item->xid,
+            'application_code' => (string)$item->application_code,
             'status' => [
-                'id' => $item->status_id,
-                'name' => $item->status_name,
+                'id' => (int)$item->status_id,
+                'name' => (string)$item->status_name,
             ],
-            'financing_object_count' => $item->financing_object_count,
-            'financing_facility_name' => $item->financing_facility_name,
-            'financing_method_name' => $item->financing_method_name,
+            'financing_object_count' => (int)$item->financing_object_count,
+            'financing_facility_name' => (string)$item->financing_facility_name,
+            'financing_method_name' => (string)$item->financing_method_name,
             'created_at' => unix_timestamp($item->created_at),
         ];
     }
