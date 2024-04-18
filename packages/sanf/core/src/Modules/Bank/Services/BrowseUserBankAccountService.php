feba8b5a52404a361c4175a25d5b939a37af9de3
nbs-diar <diar@nusantarabetastudio.com>
2024-04-18 13:12:09 +0700

    refactor: define mock source for user bank account

--- packages/sanf/core/src/Modules/Bank/Services/BrowseUserBankAccountService.php
+++ packages/sanf/core/src/Modules/Bank/Services/BrowseUserBankAccountService.php
@@ -21,7 +21,8 @@ final class BrowseUserBankAccountService implements ApplicationServiceInterface
      */
     public function execute($dto = null)
     {
-        $response = $this->coreClient->getUserBankAccount($dto->profileXid);
+        // $response = $this->coreClient->getUserBankAccount($dto->profileXid);
+        $response = json_decode('{"status":true,"code":"S_GetData","message":"Success","count":15,"data":[{"ID":"003","OWNER":"PT ABC","PROVIDER":"BCA","ACCOUNT_NUMBER":"123abc","IS_DEFAULT":true,"DATE_UPDATE":1683676800}]}', true);
 
         $data = array_map(function ($bankAccount) {
             return new GuzzleUserBankAccountEntity($bankAccount);
