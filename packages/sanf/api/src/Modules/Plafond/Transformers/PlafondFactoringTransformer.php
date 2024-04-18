05f849042c94ab8ee3ae9d538ac0ec961b8efb30
nbs-diar <diar@nusantarabetastudio.com>
2024-04-18 10:54:09 +0700

    fix: add plafond factoring transformer

---
+++ packages/sanf/api/src/Modules/Plafond/Transformers/PlafondFactoringTransformer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Sanf\Api\Modules\Plafond\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use Spatie\Fractalistic\ArraySerializer;
+
+final class PlafondFactoringTransformer extends TransformerAbstract
+{
+    public function transform($dto)
+    {
+        return [
+            'xid' => $dto->xid,
+            'submit_balance' => $dto->submitAmount,
+            'remaining_balance' => $dto->remainingAmount,
+            'used_balance' => $dto->usedAmount,
+            'customer_review' => $dto->customerReview,
+            'customers' => fractal($dto->customers)
+                ->transformWith(CustomerPlafondFactoringTransformer::class)
+                ->serializeWith(new ArraySerializer()),
+            'expired_at' => unix_timestamp($dto->expiredAt),
+            'updated_at' => unix_timestamp($dto->updatedAt),
+        ];
+    }
+}
