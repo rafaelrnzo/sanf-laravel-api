924cc7b337ef399a091e67ad6948545157f8f5c2
nbs-diar <diar@nusantarabetastudio.com>
2024-07-03 14:27:50 +0700

    fix(plafond-disbursement): handle missing temp file when attach to mail

--- packages/sanf/core/src/Modules/Plafond/Jobs/SendEmailPaymentAccelarationDocumentJob.php
+++ packages/sanf/core/src/Modules/Plafond/Jobs/SendEmailPaymentAccelarationDocumentJob.php
@@ -2,6 +2,7 @@
 
 namespace Sanf\Core\Modules\Plafond\Jobs;
 
+use Dompdf\Dompdf;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -36,6 +37,36 @@ class SendEmailPaymentAccelarationDocumentJob implements ShouldQueue
             'Perusahaan' => $this->payload->company,
         ];
 
+        $pdfFile = $this->generateFile([
+            'client' => '(PT) Pihak Pertama',
+            'customer' => '(PT) Pihak Kedua',
+            'customer_address' => 'Jl. Alamat Pihak Kedua',
+            'document_no' => 'Nomor Surat',
+            'document_date' => 'Tanggal Surat',
+            'first_signer_company' => '(PT) Pihak Pertama',
+            'first_signer_name' => 'Pejabat Pihak Pertama',
+            'first_signer_position' => 'Jabatan Pihak Pertama',
+            'second_signer_company' => '(PT) Pihak Kedua',
+            'second_signer_name' => 'Pejabat Pihak Kedua',
+            'second_signer_position' => 'Jabatan Pihak Kedua',
+            'invoices' => [
+                [
+                    'index' => '1.',
+                    'no' => 'xxxx-xxxx/xxxx/xxxx',
+                    'date' => 'dd/mm/yyyy',
+                    'amount' => 'Rp xxx.xxx,xx',
+                    'vat_amount' => 'Rp xxx.xxx,xx',
+                    'tax_amount' => 'Rp xxx.xxx,xx',
+                    'backharge_amount' => 'Rp xxx.xxx,xx',
+                    'total_amount' => 'Rp xxx.xxx,xx',
+                ],
+            ],
+            'total_amount' => 'Rp xxx.xxx,xx',
+        ]);
+
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'pdf');
+        file_put_contents($tempFilePath, $pdfFile);
+
         $adminMail = config('sanf-mobile.mail_to_admin');
         $reportUrl = "mailto:{$adminMail}?subject=Laporan Pengajuan Surat Percepatan Pencairan Plafon";
         $mailable = (new MailLayout2Columns())
@@ -61,11 +92,23 @@ class SendEmailPaymentAccelarationDocumentJob implements ShouldQueue
                 __('. Jika Anda merasa tidak membuat request tersebut mohon abaikan email ini atau anda dapat'),
                 [__('Laporkan email ini'), $reportUrl]
             )
-            ->attach($this->payload->tempFile, [
+            ->attach($tempFilePath, [
                 'as' => "Surat-Percepatan-Plafond:{$this->payload->plafondId}.pdf",
                 'mime' => 'application/pdf',
             ]);
 
         return Mail::to([$this->recipient, 'diar@nusantarabetastudio.com', 'muflih@nusantarabetastudio.com'])->send($mailable);
+    }
+
+    private function generateFile(array $content): string
+    {
+        $template = view('acc-document-template', ['content' => $content])->render();
+
+        $pdf = new Dompdf();
+        $pdf->loadHtml($template);
+        $pdf->setPaper('A4', 'potrait');
+        $pdf->render();
+
+        return $pdf->output();
     }
 }
--- packages/sanf/core/src/Modules/Plafond/UseCases/SendPaymentAccelarationDocumentUseCase.php
+++ packages/sanf/core/src/Modules/Plafond/UseCases/SendPaymentAccelarationDocumentUseCase.php
@@ -2,7 +2,6 @@
 
 namespace Sanf\Core\Modules\Plafond\UseCases;
 
-use Dompdf\Dompdf;
 use NbsPhp\Core\Exceptions\UserNotFoundException;
 use NbsPhp\Core\Repositories\UserRepositoryInterface;
 use NbsPhp\Core\Services\ApplicationServiceInterface;
@@ -26,56 +25,14 @@ final class SendPaymentAccelarationDocumentUseCase implements ApplicationService
             throw new UserNotFoundException();
         }
 
-        $pdfFile = $this->generateFile([
-            'client' => '(PT) Pihak Pertama',
-            'customer' => '(PT) Pihak Kedua',
-            'customer_address' => 'Jl. Alamat Pihak Kedua',
-            'document_no' => 'Nomor Surat',
-            'document_date' => 'Tanggal Surat',
-            'first_signer_company' => '(PT) Pihak Pertama',
-            'first_signer_name' => 'Pejabat Pihak Pertama',
-            'first_signer_position' => 'Jabatan Pihak Pertama',
-            'second_signer_company' => '(PT) Pihak Kedua',
-            'second_signer_name' => 'Pejabat Pihak Kedua',
-            'second_signer_position' => 'Jabatan Pihak Kedua',
-            'invoices' => [
-                [
-                    'index' => '1.',
-                    'no' => 'xxxx-xxxx/xxxx/xxxx',
-                    'date' => 'dd/mm/yyyy',
-                    'amount' => 'Rp xxx.xxx,xx',
-                    'vat_amount' => 'Rp xxx.xxx,xx',
-                    'tax_amount' => 'Rp xxx.xxx,xx',
-                    'backharge_amount' => 'Rp xxx.xxx,xx',
-                    'total_amount' => 'Rp xxx.xxx,xx',
-                ],
-            ],
-            'total_amount' => 'Rp xxx.xxx,xx',
-        ]);
-
-        $tempFilePath = tempnam(sys_get_temp_dir(), 'pdf');
-        file_put_contents($tempFilePath, $pdfFile);
-
         $payload = (object) [
-            'tempFile' => $tempFilePath,
             'company' => 'PIHAK PERTAMA (PT)',
             'bowheer' => 'PIHAK KEDUA (PT)',
             'plafondId' => 'PLAFOND NO',
         ];
+
         dispatch(new SendEmailPaymentAccelarationDocumentJob($user->username, $payload));
 
         return true;
-    }
-
-    private function generateFile(array $content)
-    {
-        $template = view('acc-document-template', ['content' => $content])->render();
-
-        $pdf = new Dompdf();
-        $pdf->loadHtml($template);
-        $pdf->setPaper('A4', 'potrait');
-        $pdf->render();
-
-        return $pdf->output();
     }
 }
