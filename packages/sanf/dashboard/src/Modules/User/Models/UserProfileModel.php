5e7d06c4d9bb55b68f98a20269c56b97d078ae83
nbs-diar <diar@nusantarabetastudio.com>
2024-07-05 08:59:41 +0700

    refactor(model): using soft delete at auth and profile

--- packages/sanf/dashboard/src/Modules/User/Models/UserProfileModel.php
+++ packages/sanf/dashboard/src/Modules/User/Models/UserProfileModel.php
@@ -2,12 +2,16 @@
 
 namespace Sanf\Dashboard\Modules\User\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use NbsPhp\Core\Models\AbstractModel;
 
 class UserProfileModel extends AbstractModel
 {
+    use SoftDeletes;
+
     const CREATED_AT = 'createdAt';
     const UPDATED_AT = 'updatedAt';
+    const DELETED_AT = 'deletedAt';
 
     protected $connection = 'dashboard_db';
 
