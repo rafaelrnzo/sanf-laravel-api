3949bb841a57da70c99f7f9a60c19944bff81b12
nbs-diar <diar@nusantarabetastudio.com>
2024-08-27 08:55:32 +0700

    refactor(html): add noopener in url tag

--- packages/sanf/web/resources/views/web-view/e-sign/terms-and-condition-vida.php
+++ packages/sanf/web/resources/views/web-view/e-sign/terms-and-condition-vida.php
@@ -1,41 +1,44 @@
 <!DOCTYPE html>
 <html lang="id">
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Syarat dan Ketentuan - Digital Signature VIDA</title>
     <style>
-      body {
-        font-family: Arial, sans-serif;
-        line-height: 1.6;
-        display: flex;
-        justify-content: center;
-      }
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            display: flex;
+            justify-content: center;
+        }
 
-      h2 {
-          text-transform: uppercase;
-          text-align: center;
-      }
+        h2 {
+            text-transform: uppercase;
+            text-align: center;
+        }
 
-      .container {
-        width: 210mm;
-        /* A4 width */
-        height: 297mm;
-        /* A4 height */
-      }
+        .container {
+            width: 210mm;
+            /* A4 width */
+            height: 297mm;
+            /* A4 height */
+        }
 
-      p {
-        text-align: justify;
-      }
+        p {
+            text-align: justify;
+        }
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
-      <h2>Syarat dan Ketentuan <br> Digital Signature VIDA </h2>
-      <p> Sehubungan dengan verifikasi identitas untuk tujuan pembubuhan tanda tangan elektronik berbasis sertifikat elektronik, data pribadi Anda berupa data demografi dan/atau biometrik akan diperiksa kesesuaiannya, oleh PT Indonesia Digital Identity (VIDA) sebagai mitra kami, dengan data yang tercatat pada sistem instansi pemerintahan yang berhak mengeluarkan identitas tersebut. Apabila data pribadi Anda terverifikasi kesesuaiannya, maka VIDA sebagai Penyelenggara Sertifikasi Elektronik tersertifikasi oleh Kementerian Telekomunikasi dan Informasi, akan menerbitkan sertifikat elektronik sebagai bukti bahwa data pribadi Anda telah diverifikasi dan sesuai dengan data yang tercatat pada sistem instansi yang berhak mengeluarkan identitas tersebut. </p>
-      <p> Oleh karenanya, Anda menjamin keakuratan data pribadi yang Anda sediakan dan setuju atas pemrosesan data pribadi Anda tersebut untuk tujuan penerbitan sertifikat elektronik serta layanan lain yang melekat pada sertifikat elektronik yang dilakukan oleh VIDA. </p>
-      <p> Anda telah membaca, memahami, dan setuju untuk terikat pada syarat dan ketentuan layanan Penyelenggara Sertifikasi Elektronik yang terdapat pada Perjanjian Kepemilikan Sertifikat Elektronik (<a href="https://dl.vida.id/VIDA-Subscriber_Agreement.pdf" target="_blank">Subscriber Agreement</a>), Kebijakan Privasi PSrE (<a href="https://dl.vida.id/VIDA-Privacy_Policy_for_Digital_Certificate.pdf" target="_blank">CA Privacy Policy</a>), serta Pernyataan Penyelenggaraan Sertifikasi Elektronik (<a href="https://dl.vida.id/VIDA-CPS.pdf" target="_blank">Certification Practice Statement</a>) VIDA yang dapat diakses melalui: <a href="https://repo.vida.id" target="_blank">https://repo.vida.id</a>
-      </p>
+        <h2>Syarat dan Ketentuan <br> Digital Signature VIDA </h2>
+        <p> Sehubungan dengan verifikasi identitas untuk tujuan pembubuhan tanda tangan elektronik berbasis sertifikat elektronik, data pribadi Anda berupa data demografi dan/atau biometrik akan diperiksa kesesuaiannya, oleh PT Indonesia Digital Identity (VIDA) sebagai mitra kami, dengan data yang tercatat pada sistem instansi pemerintahan yang berhak mengeluarkan identitas tersebut. Apabila data pribadi Anda terverifikasi kesesuaiannya, maka VIDA sebagai Penyelenggara Sertifikasi Elektronik tersertifikasi oleh Kementerian Telekomunikasi dan Informasi, akan menerbitkan sertifikat elektronik sebagai bukti bahwa data pribadi Anda telah diverifikasi dan sesuai dengan data yang tercatat pada sistem instansi yang berhak mengeluarkan identitas tersebut. </p>
+        <p> Oleh karenanya, Anda menjamin keakuratan data pribadi yang Anda sediakan dan setuju atas pemrosesan data pribadi Anda tersebut untuk tujuan penerbitan sertifikat elektronik serta layanan lain yang melekat pada sertifikat elektronik yang dilakukan oleh VIDA. </p>
+        <p> Anda telah membaca, memahami, dan setuju untuk terikat pada syarat dan ketentuan layanan Penyelenggara Sertifikasi Elektronik yang terdapat pada Perjanjian Kepemilikan Sertifikat Elektronik (<a href="https://dl.vida.id/VIDA-Subscriber_Agreement.pdf" target="_blank" rel="noopener">Subscriber Agreement</a>), Kebijakan Privasi PSrE (<a href="https://dl.vida.id/VIDA-Privacy_Policy_for_Digital_Certificate.pdf" target="_blank" rel="noopener">CA Privacy Policy</a>), serta Pernyataan Penyelenggaraan Sertifikasi Elektronik (<a href="https://dl.vida.id/VIDA-CPS.pdf" target="_blank" rel="noopener">Certification Practice Statement</a>) VIDA yang dapat diakses melalui: <a href="https://repo.vida.id" target="_blank" rel="noopener">https://repo.vida.id</a>
+        </p>
     </div>
-  </body>
+</body>
+
 </html>
