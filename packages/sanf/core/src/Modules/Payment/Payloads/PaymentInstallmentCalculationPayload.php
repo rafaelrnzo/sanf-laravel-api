86a3775ae41134d3d3e4f2057e7c446dbf5f4ade
Ikram Lubis <ikram@nusantarabetastudio.com>
2025-12-30 03:00:30 +0700

    fix(payment): error datatype payload

--- packages/sanf/core/src/Modules/Payment/Payloads/PaymentInstallmentCalculationPayload.php
+++ packages/sanf/core/src/Modules/Payment/Payloads/PaymentInstallmentCalculationPayload.php
@@ -2,15 +2,18 @@
 
 namespace Sanf\Core\Modules\Payment\Payloads;
 
+use Sanf\Core\Traits\CastsNumericDtoProperties;
 use Spatie\DataTransferObject\DataTransferObject;
 
 class PaymentInstallmentCalculationPayload extends DataTransferObject
 {
+    use CastsNumericDtoProperties;
+
     public string $profileXid;
     public ?int $customAmount;
     public ?int $customPenaltyAmount;
     /**
-     * @var array|PaymentInstallmentPayload[]
+     * @var \Sanf\Core\Modules\Payment\Payloads\PaymentInstallmentPayload[]
      */
     public array $installments;
     public bool $preferCache = false;
