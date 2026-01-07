fbffb8e3a32ecabd6e7fdb4f6d9b9b3c2f1fb8d1
Ikram Lubis <ikram@nusantarabetastudio.com>
2026-01-07 15:17:54 +0700

    fix(installment/detail): handle nullable data from core

--- packages/sanf/integration/src/Modules/SanfCore/Entities/SanfCoreInstallmentDetailContractEntity.php
+++ packages/sanf/integration/src/Modules/SanfCore/Entities/SanfCoreInstallmentDetailContractEntity.php
@@ -9,10 +9,10 @@ class SanfCoreInstallmentDetailContractEntity extends FlexibleDataTransferObject
 {
     use CastsNumericDtoProperties;
 
-    public string $no_kontrak;
+    public ?string $no_kontrak;
     public ?string $status_kontrak_id;
     public ?string $status_kontrak_desc;
-    public string $tanggal_kontrak;
+    public ?string $tanggal_kontrak;
     public ?string $nama_supplier;
     public ?string $tipe_pembayaran_id;
     public ?string $tipe_pembayaran_desc;
@@ -20,16 +20,16 @@ class SanfCoreInstallmentDetailContractEntity extends FlexibleDataTransferObject
     public ?string $jenis_pembiayaan_desc;
     public ?string $cara_pembiayaan_id;
     public ?string $cara_pembiayaan_desc;
-    public string $tenor;
-    public string $tipe_tenor;
-    public string $jatuh_tempo; // 'Y-m-d' format ": "2025-12-21",
+    public ?string $tenor;
+    public ?string $tipe_tenor;
+    public ?string $jatuh_tempo; // 'Y-m-d' format ": "2025-12-21",
     public ?string $tgl_selesai; // 'Y-m-d' format": null,
-    public float $bunga_harian;
-    public float $total_pembiayaan;
-    public int $schedule_no; // angsuran ke ...
-    public int $schedule_total; // dari ... angsuran
-    public float $af_amount; // Nilai pembiayaan
-    public float $dp_amount; // DP dibayarkan
-    public float $ar_paid; // Nominal sudah dibayarkan
-    public float $ar_outs; // Nominal sisa
+    public ?float $bunga_harian;
+    public ?float $total_pembiayaan;
+    public ?int $schedule_no; // angsuran ke ...
+    public ?int $schedule_total; // dari ... angsuran
+    public ?float $af_amount; // Nilai pembiayaan
+    public ?float $dp_amount; // DP dibayarkan
+    public ?float $ar_paid; // Nominal sudah dibayarkan
+    public ?float $ar_outs; // Nominal sisa
 }
