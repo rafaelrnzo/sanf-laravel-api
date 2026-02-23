12446dc4f95cb61cc1db6892719ce52fac26eca2
Ikram Lubis <ikram@nusantarabetastudio.com>
2026-02-23 15:09:46 +0700

    feat(plafond-sparepart): update remaining plafond formula

--- packages/sanf/api/src/Modules/Plafond/Transformers/PlafondSparePartTransformer.php
+++ packages/sanf/api/src/Modules/Plafond/Transformers/PlafondSparePartTransformer.php
@@ -13,8 +13,8 @@ final class PlafondSparePartTransformer extends TransformerAbstract
     {
         return [
             'xid' => $data->no_plafond,
-            'submit_balance' => (float) $data->p_total,
-            'remaining_balance' => (float) $data->p_sisa,
+            'submit_balance' => $total = (float) $data->p_total,
+            'remaining_balance' => $total - (float) $data->p_sisa,
             'supplier' => [
                 'id' => $data->tipe_supplier,
                 'name' => $data->nama_supplier,
